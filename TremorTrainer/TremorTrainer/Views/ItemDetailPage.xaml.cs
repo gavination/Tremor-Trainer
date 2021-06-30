3cf77e9b1d804b88eb1aef9fd821b760f1a6b106
Gavin <gavinbauman1322@gmail.com>
2021-06-30 19:45:21 -0400

    Add new project scaffold

---
+++ TremorTrainer/TremorTrainer/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TremorTrainer.ViewModels;
+using Xamarin.Forms;
+
+namespace TremorTrainer.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
