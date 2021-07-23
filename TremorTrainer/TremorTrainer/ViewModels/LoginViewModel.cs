eaab930a4f5fb175144ac236f818206fdf70ea6e
Gavination <gavinbauman1322@gmail.com>
2021-07-23 01:23:02 -0400

    Add background image for login screen

--- TremorTrainer/TremorTrainer/ViewModels/LoginViewModel.cs
+++ TremorTrainer/TremorTrainer/ViewModels/LoginViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using TremorTrainer.Views;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace TremorTrainer.ViewModels
 {
