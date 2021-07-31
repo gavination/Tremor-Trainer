c6501d126df3e2deccc0462939ea1a287fcfb697
Gavination <gavinbauman1322@gmail.com>
2021-07-31 13:54:07 -0400

    extract timer code to its own service.

---
+++ TremorTrainer/TremorTrainer/Services/TimerService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TremorTrainer.Services
+{
+    public class TimerService : ITimerService
+    {
+        private Timer _timer;
+        private TimeSpan _span;
+        private bool _isSessionRunning;
+        private IMessageService _messageService;
+        private readonly int _interval;
+
+        public TimeSpan Span => _span;
+        public Timer Timer => _timer;
+
+
+        public TimerService(IMessageService messageService)
+        {
+            _timer = new Timer();
+            _isSessionRunning = false;
+            _interval = Constants.CountdownInterval;
+        }
+
+        public string FormatTimeSpan(TimeSpan span)
+        {
+            return $"Time Remaining: {(int)span.TotalMinutes}:{(int)span.TotalSeconds}";
+        }
+
+        public async Task StartTimerAsync(int timerLength)
+        {
+            if (timerLength > 0 && !_isSessionRunning)
+            {
+                //trigger a timer event every for every interval that passes 
+                _timer.Enabled = true;
+                _isSessionRunning = true;
+            }
+            else if (!_isSessionRunning && timerLength <= 0)
+            {
+                //reset the session length to start a new one
+                timerLength = (int)App.Current.Properties["SessionLength"];
+                _timer = new Timer(_interval);
+                _isSessionRunning = true;
+
+                //trigger a timer event every for every interval that passes
+                _timer.Enabled = true;
+            }
+            else
+            {
+                // session is still active if boolean is true
+                await _messageService.ShowAsync("Session is already active.");
+            }
+        }
+
+        public async Task StopTimerAsync()
+        {
+            try
+            {
+                _timer.Stop();
+                _timer.Enabled = false;
+                _timer.Dispose();
+            }
+            catch (Exception e)
+            {
+                var message = $"{Constants.UnknownErrorMessage}. Details: {e.Message}";
+                Console.WriteLine(message);
+                await _messageService.ShowAsync(Constants.UnknownErrorMessage);
+            }
+
+        }
+    }
+
+    public interface ITimerService
+    {
+        Timer Timer { get; }
+        string FormatTimeSpan(TimeSpan span);
+        Task StartTimerAsync(int timerLength);
+        Task StopTimerAsync();
+    }
+}
--- TremorTrainer/TremorTrainer/ViewModels/AccelerometerViewModel.cs
+++ TremorTrainer/TremorTrainer/ViewModels/AccelerometerViewModel.cs
@@ -18,7 +18,8 @@ namespace TremorTrainer.ViewModels
         private string _readingText = "Placeholder XYZ values";
         private string _timerText;
         private readonly IMessageService _messageService;
-        private readonly ISessionService _sessionService;   
+        private readonly ISessionService _sessionService;
+        private readonly ITimerService _timerService;
         private int _sessionLength;
         private Timer _sessiontimer;
         private DateTime _startTime;
@@ -29,15 +30,21 @@ namespace TremorTrainer.ViewModels
         public string ReadingText => _readingText;
         public string TimerText => _timerText;
 
-        public AccelerometerViewModel(IMessageService messageService, ISessionService sessionService)
+        public AccelerometerViewModel(IMessageService messageService, ISessionService sessionService, ITimerService timerService)
         {
+
+            // Fulfill external services 
+            _messageService = messageService;
+            _sessionService = sessionService;
+            _timerService = timerService;
+
             // ViewModel Page Setup
             // Setup UI elements and register propertychanged events
             Title = "Start Training";
             _sessionLength = (int)App.Current.Properties["SessionLength"];
             _sessiontimer = new Timer(_interval);
-            TimeSpan timespan = TimeSpan.FromMilliseconds(_sessionLength);
-            _timerText = $"Time Remaining: {(int)timespan.TotalMinutes}:{(int)timespan.TotalSeconds}";
+
+            _timerText = _timerService.FormatTimeSpan(TimeSpan.FromMilliseconds(_sessionLength));
             OnPropertyChanged("TimerText");
             _accelerometerReadings = new List<Vector3>();
 
@@ -45,11 +52,10 @@ namespace TremorTrainer.ViewModels
             StartSessionCommand = new Command(async () => await StartAccelerometer());
             Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
 
-            ViewResultsCommand = new Command(async () => await Shell.Current.GoToAsync("/ItemsPage"));
+            // Register timer elapsed event 
+            _timerService.Timer.Elapsed += OnTimedEvent;
 
-            // Fulfill external services 
-            _messageService = messageService;
-            _sessionService = sessionService;
+            ViewResultsCommand = new Command(async () => await Shell.Current.GoToAsync("/ItemsPage"));
         }
 
         private async Task SaveSessionAsync(Vector3 reading, TimeSpan duration)
@@ -108,7 +114,7 @@ namespace TremorTrainer.ViewModels
                     Accelerometer.Start(Constants.SensorSpeed);
                     
                     // Start timer and get a Stamp of when Session began
-                    await StartTimerAsync();
+                    await _timerService.StartTimerAsync(_sessionLength);
                     _startTime = DateTime.Now;
                 }
             }
@@ -150,50 +156,6 @@ namespace TremorTrainer.ViewModels
             }
         }
 
-        private async Task StartTimerAsync()
-        {
-            if (_sessionLength > 0 && !_isSessionRunning)
-            {
-                //trigger a timer event every for every interval that passes 
-                _sessiontimer.Elapsed += OnTimedEvent;
-                _sessiontimer.Enabled = true;
-                _isSessionRunning = true;
-            }
-            else if (!_isSessionRunning && _sessionLength <= 0)
-            {
-                //reset the session length to start a new one
-                _sessionLength = (int)App.Current.Properties["SessionLength"];
-                _sessiontimer = new Timer(_interval);
-                _isSessionRunning = true;
-
-                //trigger a timer event every for every interval that passes
-                _sessiontimer.Elapsed += OnTimedEvent;
-                _sessiontimer.Enabled = true;
-            }
-            else
-            {
-                // session is still active if boolean is true
-                await _messageService.ShowAsync("Session is already active.");
-            }
-
-        }
-
-        private async Task StopTimer()
-        {
-            try
-            {
-                _sessiontimer.Stop();
-                _sessiontimer.Enabled = false;
-                _sessiontimer.Dispose();
-            }
-            catch (Exception e)
-            {
-                var message = $"{Constants.UnknownErrorMessage}. Details: {e.Message}";
-                Console.WriteLine(message);
-                await _messageService.ShowAsync(Constants.UnknownErrorMessage);
-            }
-
-        }
 
         private async void OnTimedEvent(object sender, ElapsedEventArgs e)
         {
@@ -212,7 +174,7 @@ namespace TremorTrainer.ViewModels
                 OnPropertyChanged("TimerText");
 
                 //stop the timer, saves the result. resets the _sessionRunning flag
-                await StopTimer();
+                await _timerService.StopTimerAsync();
                 await StopAccelerometer();
                 var averageReading = GetAverageReading();
 
