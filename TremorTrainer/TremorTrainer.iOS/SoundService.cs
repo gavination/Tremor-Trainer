cb82f9f62982abe1db9e2ce725fdf9c6dd37a245
Gavination <gavinbauman1322@gmail.com>
2022-08-19 19:22:44 -0400

    Add reference to ios sound service

--- TremorTrainer/TremorTrainer.iOS/SoundService.cs
+++ TremorTrainer/TremorTrainer.iOS/SoundService.cs
@@ -3,29 +3,38 @@ using System.IO;
 using System.Threading.Tasks;
 using AVFoundation;
 using Foundation;
+using TremorTrainer.iOS;
 using TremorTrainer.Services;
 using Xamarin.Forms;
 
+[assembly: Dependency(typeof(SoundService))]
 namespace TremorTrainer.iOS
 {
     public class SoundService : ISoundService
     {
 
-        AVAudioPlayer _player;
+        private AVAudioPlayer _player;
+        private string fileName;
+        private NSUrl url;
 
+        public SoundService()
+        {
+            fileName = "metronomeding.mp3";
+            string sFilePath = NSBundle.MainBundle.PathForResource
+                (Path.GetFileNameWithoutExtension(fileName), Path.GetExtension(fileName));
 
+            url = NSUrl.FromString(sFilePath);
+
+        }
         public Task playSound()
         {
-            var fileName = "metronomeding.mp3";
-            string sFilePath = NSBundle.MainBundle.PathForResource
-              (Path.GetFileNameWithoutExtension(fileName), Path.GetExtension(fileName));
-            var url = NSUrl.FromString(sFilePath);
             _player = AVAudioPlayer.FromUrl(url);
+            _player.Play();
+
             _player.FinishedPlaying += (object sender, AVStatusEventArgs e) =>
             {
                 _player = null;
             };
-            _player.Play();
             return Task.CompletedTask;
         }
     }
