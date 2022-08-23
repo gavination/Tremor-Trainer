3643bff4c60ded16c1c07d86df94f5e4c2d4ac0c
Gavination <gavinbauman1322@gmail.com>
2022-08-22 22:08:40 -0400

    copy build script to ios proj

---
+++ TremorTrainer/TremorTrainer.iOS/appcenter-pre-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This script runs after the initial clone of the repo, but before the build process starts
+# It will inject the VS App Center secrets necessary for collecting app telemetry
+# It assumes the machine running has the AndroidAppCenterId and IOSAppCenterId environment vars set
+
+# Install JQ for modifying the template.appsettings.json file. 
+sudo apt-get -y install jq
+
+
+# Pipe template into the appsettings file and replace values with proper secrets
+cd ../TremorTrainer
+cat template.appsettings.json | jq \
+ --arg androidAppCenterId  "$AndroidAppCenterId" \
+ --arg iosAppCenterId "$IOSAppCenterId" \
+ '.IOSAppCenterId=$iosAppCenterId| .AndroidAppCenterId=$androidAppCenterId' > appsettings.json
